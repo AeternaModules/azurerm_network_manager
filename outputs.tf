3a435d99478e0d7cbec6722090e37226523070bd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "network_managers" {
-  description = "All network_manager resources"
-  value       = azurerm_network_manager.network_managers
-}
 output "network_managers_cross_tenant_scopes" {
-  description = "List of cross_tenant_scopes values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.cross_tenant_scopes]
+  description = "Map of cross_tenant_scopes values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.cross_tenant_scopes }
 }
 output "network_managers_description" {
-  description = "List of description values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.description]
+  description = "Map of description values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.description }
 }
 output "network_managers_location" {
-  description = "List of location values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.location]
+  description = "Map of location values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.location }
 }
 output "network_managers_name" {
-  description = "List of name values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.name]
+  description = "Map of name values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.name }
 }
 output "network_managers_resource_group_name" {
-  description = "List of resource_group_name values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.resource_group_name]
+  description = "Map of resource_group_name values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.resource_group_name }
 }
 output "network_managers_scope" {
-  description = "List of scope values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.scope]
+  description = "Map of scope values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.scope }
 }
 output "network_managers_scope_accesses" {
-  description = "List of scope_accesses values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.scope_accesses]
+  description = "Map of scope_accesses values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.scope_accesses }
 }
 output "network_managers_tags" {
-  description = "List of tags values across all network_managers"
-  value       = [for k, v in azurerm_network_manager.network_managers : v.tags]
+  description = "Map of tags values across all network_managers, keyed the same as var.network_managers"
+  value       = { for k, v in azurerm_network_manager.network_managers : k => v.tags }
 }
 
